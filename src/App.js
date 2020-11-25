7813fceab906b90dedd0c393206e3bb6716ab38b
Prakashandrews <prakashandrews@gmail.com>
2020-11-25 22:50:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import EmployeeDetails from './components/EmployeeDetails';
-
 import {
   BrowserRouter as Router,
   Switch,
